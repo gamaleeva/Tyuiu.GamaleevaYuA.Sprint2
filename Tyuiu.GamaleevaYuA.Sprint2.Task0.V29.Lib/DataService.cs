ba887a1f06f40dc4c0559d50f42cb08419a1c9be
gamaleeva <gamaleevacin@mail.ru>
2025-10-04 19:24:11 +0300

    Task 0

--- Tyuiu.GamaleevaYuA.Sprint2.Task0.V29.Lib/DataService.cs
+++ Tyuiu.GamaleevaYuA.Sprint2.Task0.V29.Lib/DataService.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.GamaleevaYuA.Sprint2.Task0.V29.Lib
 {
-    public class DataService
+    public class DataService : ISprint2Task0V29
     {
         public bool[] GetCompareOperations(int x, int y)
         {
@@ -10,13 +10,12 @@ namespace Tyuiu.GamaleevaYuA.Sprint2.Task0.V29.Lib
             res[0] = x == y;
             res[1] = x + 630 != y;
             res[2] = x < y;
-            res[3] = x +  631 > y;
+            res[3] = x + 631 > y;
             res[4] = x + 631 <= y;
             res[5] = x >= y;
 
             return res;
-
-
         }
     }
 }
+
